74f996d994f6c6314109667367067df311a4d7f0
kchang00 <k_chang@fanshaweonline.ca>
2020-03-03 20:41:44 -0500

    cleaned up files and added references

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
     require_once 'load.php';
-
-    if(isset($_GET['filter'])){
-        // work around for if you have too many parameters - an array
-        // also ensures you don't have to use all the parameters or in the same order
-        $args = array(
-            'tbl'=>'tbl_movies',
-            'tbl2'=>'tbl_genre',
-            'tbl3'=>'tbl_mov_genre',
-            'col'=>'movies_id',
-            'col2'=>'genre_id',
-            'col3'=>'genre_name',
-            'filter'=>$_GET['filter']
-        );
-        $getMovies = getMoviesByFilter($args);
-    }else{
-        $movie_table = 'tbl_movies';
-        $getMovies = getAll($movie_table);
-    }
 ?>
 
 <!DOCTYPE html>
@@ -26,18 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movie CMS</title>
+    <title>Home</title>
 </head>
 <body>
-    <?php include 'templates/header.php';?>
-    <?php while($row = $getMovies->fetch(PDO::FETCH_ASSOC)):?>
-    <div class="movie-item">
-        <h2><?php echo $row['movies_title'];?></h2>
-        <h4><?php echo $row['movies_year'];?></h4>
-        <img src="images/<?php echo $row['movies_cover'];?>" alt="<?php echo $row['movies_title'];?>"/>
-        <a href="details.php?id=<?php echo $row['movies_id'];?>">Read More</a>
-    </div>
-    <?php endwhile;?>
+    <main>
+        <h1>Home</h1>
+        <a href="admin/index.php">Go to Login</a>
+    </main>
 </body>
 <?php include 'templates/footer.php';?>
 </html>
