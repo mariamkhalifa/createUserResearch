3be8ac6b10d62bb018ee5c37e2fe8d7a4e71ad6d
kchang00 <k_chang@fanshaweonline.ca>
2020-03-03 21:18:41 -0500

    hashed pass and pass generator lgoic

--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -1,7 +1,28 @@
 <?php
 
+// usage: $newpassword = generatePassword(12); // for a 12-char password, upper/lower/numbers.
+// functions that use rand() or mt_rand() are not secure according to the PHP manual.
+
+function getRandomBytes($nbBytes = 32){
+    $bytes = openssl_random_pseudo_bytes($nbBytes, $strong);
+    if (false !== $bytes && true === $strong) {
+        return $bytes;
+    }
+    else {
+        throw new \Exception("Unable to generate secure token from OpenSSL.");
+    }
+}
+
+function generatePassword($length){
+    return substr(preg_replace("/[^a-zA-Z0-9]/", "", base64_encode(getRandomBytes($length+1))),0,$length);
+}
+
 function createUser($fname, $username, $password, $email) {
     $pdo = Database::getInstance()->getConnection();
+    $new_password = generatePassword(8);
+    $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+    var_dump($hashed_password);
+    var_dump($new_password);
 
     //To Do: build the proper SQL query with the information above
     // execute it to create a user in tbl_user
@@ -13,7 +34,7 @@ function createUser($fname, $username, $password, $email) {
         array(
             ':fname'=>$fname,
             ':username'=>$username,
-            ':password'=>$password,
+            ':password'=>$hashed_password,
             ':email'=>$email
         )
     ); 
@@ -25,6 +46,6 @@ function createUser($fname, $username, $password, $email) {
     if($create_user_result) {
         redirect_to('index.php');
     }else{
-        return 'This individul sucks!';
+        return 'This individul sucks! But they are trying their best :)';
     }
 }
