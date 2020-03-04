35e086d7d6e49219d4408453ea5d8f6b96c4ddb1
kchang00 <k_chang@fanshaweonline.ca>
2020-03-04 11:31:55 -0500

    mail function tests

--- admin/admin_createuser.php
+++ admin/admin_createuser.php
@@ -29,7 +29,7 @@
     <form action="admin_createuser.php" method="post">
         <label>First Name:</label>
         <input type="text" name="fname" value=""><br>
-        <label>Username::</label>
+        <label>Username:</label>
         <input type="text" name="username" value=""><br>
         <label>Email:</label>
         <input type="email" name="email" value=""><br>
